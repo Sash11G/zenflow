732e045d5ba0f6f853ba189f2c379254d7bc4780
Sash11G <sash1133g@gmail.com>
2025-05-29 14:12:45 -0300

    daily journal, reflections

---
+++ READ_JOURNAL/new_entry.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+JOURNAL_DIR="$HOME/zenflow/READ_JOURNAL"
+
+# Get the next available number by finding the max and adding 1
+last_number=$(find "$JOURNAL_DIR" -name "day_*.txt" | sed -E 's/.*day_([0-9]+)\.txt/\1/' | sort -n | tail -n 1)
+next_number=$((last_number + 1))
+
+# Format the filename
+filename="day_${next_number}.txt"
+filepath="${JOURNAL_DIR}/${filename}"
+
+# Create the new file with the header
+{
+    date
+    echo ""
+    echo "Day ${next_number}:"
+    echo ""
+} > "$filepath"
+
+# Open it with your editor (e.g., nvim)
+nvim "$filepath"
