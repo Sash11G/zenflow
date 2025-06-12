329e0e96f69cafa3858869aba5548d35b05e776d
Sash11G <sash1133g@gmail.com>
2025-06-12 19:20:03 -0300

    Auto update at Thu Jun 12 07:20:03 PM -03 2025

--- READ_JOURNAL/new_entry.sh
+++ READ_JOURNAL/new_entry.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-JOURNAL_DIR="$HOME/zenflow/READ_JOURNAL"
+
+read -p "Enter directory name: " dir
+mkdir -p "$HOME/$dir"
+
+echo "Directory ensured: $HOME/$dir"
+JOURNAL_DIR="$HOME/zenflow/READ_JOURNAL/caffein_log"
 
 # Get the next available number by finding the max and adding 1
 last_number=$(find "$JOURNAL_DIR" -name "day_*.txt" | sed -E 's/.*day_([0-9]+)\.txt/\1/' | sort -n | tail -n 1)
