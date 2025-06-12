fa94c4e2af4f1b2fc97b5bd6a9f3173c19688c8d
Sash11G <sash1133g@gmail.com>
2025-06-12 20:36:03 -0300

    Auto update at Thu Jun 12 08:36:03 PM -03 2025

--- regional_sur/sand.py
+++ regional_sur/sand.py
@@ -1,15 +1,6 @@
-import json
-
-with open("new_data.json", "r") as f:
-    data = json.load(f)
 
 
-def trav(node, depth=0):
-    if isinstance(node, dict):
-        for key, val in node.items():
-            print("  " * depth + f"{key} {'Category' if depth == 0   else 'Subcategory'}")
-            trav(val, depth + 1)
-    elif isinstance(node, list):
-        print("  " * depth + f"{len(node)} Products")
+dict1 = {1: 1, 2: 2}
 
-trav(data)
+
+print(isinstance(dict1, dict))
